2143b546238579d5db243e68b063ca287ea7479f
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-26 11:45:13 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('It works!');
+});
+
+app.listen(3000, () => {
+  console.log(`Server is active on port 3000....`);
+});
