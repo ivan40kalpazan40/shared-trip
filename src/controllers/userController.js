75f89f014535f83ad7516844ed97187713433a73
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-26 12:19:52 +0300

    render login and register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+const renderRegister = (req, res) => {
+  res.render('user/register');
+};
+
+const renderLogin = (req, res) => {
+  res.render('user/login');
+};
+
+router.get('/register', renderRegister);
+router.get('/login', renderLogin);
 module.exports = router;
